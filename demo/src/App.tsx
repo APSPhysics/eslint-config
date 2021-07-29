37d5566a1ddc7f2c306f2adcfcd3e1d28f5b5afb
gojutin <gojutin@gmail.com>
2021-07-29 10:27:30 -0400

    test: updated and renamed demo app, added test lines, dep updates

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -2,11 +2,19 @@
 import file from "./file";
 import React, { useState } from "react";
 
+const x = 3;
+
 function App() {
   // Unexpected let, use const instead.eslintfunctional/no-let
   let x = 3;
 
+  var hello;
+
+  // Unexpected console statement.eslintno-console
   console.log(file);
+  // warn and error is fine
+  console.warn("Warning!");
+  console.error("oops");
 
   return (
     <div>
---
+++ demo/src/file.tsx
@@ -0,0 +1,2 @@
+// Assign object to a variable before exporting as module defaulteslintimport/no-anonymous-default-export
+export default {};
