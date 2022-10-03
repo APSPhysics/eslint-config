ecfbdd06eeab93530928424f86ca4147b6b17e68
Mike Short <mikeshort2014@gmail.com>
2022-10-03 17:53:05 -0400

    feat: ban most ts comments

--- index.js
+++ index.js
@@ -28,6 +28,11 @@ module.exports = {
     "prettier/prettier": "error",
     "arrow-body-style": "off",
     "prefer-arrow-callback": "off",
+    // only allow descriptive ts-expect-errors. Full Explanation https://github.com/APSPhysics/eslint-config/issues/5
+    "@typescript-eslint/ban-ts-comment": [
+      2,
+      { "ts-expect-error": "allow-with-description" },
+    ],
   },
 };
 
