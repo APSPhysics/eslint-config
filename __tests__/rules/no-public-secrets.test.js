def69e6a763647f122f6678af348d05276480e05
Mike Short <mikeshort2014@gmail.com>
2022-10-03 13:54:59 -0400

    test: no-public-secrets

---
+++ __tests__/rules/no-public-secrets.test.js
@@ -0,0 +1,67 @@
+const { noPublicSecrets } = require("../../rules/no-public-secrets");
+const { RuleTester } = require("eslint");
+
+const ruleTester = new RuleTester();
+
+const validCases = [
+  "process.env.SECRET",
+  "process.env.SECRET_KEY",
+  "process.env.API_SECRET",
+  "process.env.API_SECRET_KEY",
+];
+
+const invalidCases = [
+  // Gatsby
+  "GATSBY_SECRET",
+  "GATSBY_SECRET_KEY",
+  "GATSBY_API_SECRET",
+  "GATSBY_API_SECRET_KEY",
+  "GATSBY_PASSWORD",
+  "GATSBY_PASSWORD_KEY",
+  "GATSBY_API_PASSWORD",
+  "GATSBY_API_PASSWORD_KEY",
+  "GATSBY_PW",
+  "GATSBY_PW_KEY",
+  "GATSBY_API_PW",
+  "GATSBY_API_PW_KEY",
+  // NextJS
+  "NEXT_PUBLIC_SECRET",
+  "NEXT_PUBLIC_SECRET_KEY",
+  "NEXT_PUBLIC_API_SECRET",
+  "NEXT_PUBLIC_API_SECRET_KEY",
+  "NEXT_PUBLIC_API_PW",
+  "NEXT_PUBLIC_API_PW_KEY",
+  "NEXT_PUBLIC_PW",
+  "NEXT_PUBLIC_PW_KEY",
+  "NEXT_PUBLIC_API_PASSWORD",
+  "NEXT_PUBLIC_API_PASSWORD_KEY",
+  "NEXT_PUBLIC_PASSWORD",
+  "NEXT_PUBLIC_PASSWORD_KEY",
+  // Astro
+  "PUBLIC_SECRET",
+  "PUBLIC_SECRET_KEY",
+  "PUBLIC_API_SECRET",
+  "PUBLIC_API_SECRET_KEY",
+  "PUBLIC_API_PASSWORD",
+  "PUBLIC_API_PASSWORD_KEY",
+  "PUBLIC_PASSWORD",
+  "PUBLIC_PASSWORD_KEY",
+  "PUBLIC_PW",
+  "PUBLIC_PW_KEY",
+  "PUBLIC_API_PW",
+  "PUBLIC_API_PW_KEY",
+];
+
+const toCode = (testCase) => ({ code: testCase });
+
+const valid = validCases.map(toCode);
+
+const invalid = invalidCases
+  .map(toCode)
+  .map((code) => ({ ...code, errors: [{ messageId: "noPublicSecrets" }] }));
+
+// This runs tests that jest can interpret. Should not be nested in a `describe` function.
+ruleTester.run("no-public-secrets", noPublicSecrets, {
+  valid,
+  invalid,
+});
